f08fab90457ccd4605f3a260db7f89f81f77435e
AnastasiiaVld <anastasiia94cherniavska@gmail.com>
2023-05-03 16:16:04 +0200

    replace localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,22 @@ export class App extends Component {
 
   state = {
     contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ],
   filter: "",
+  }
+
+  componentDidMount() {
+    const contactList = JSON.parse(localStorage.getItem('contacts'))
+    if(contactList) this.setState({contacts: contactList})
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts)
+      localStorage.setItem('contacts',JSON.stringify(this.state.contacts) )
   }
 
   addContact = (contact) => {
